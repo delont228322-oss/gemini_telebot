623799d89e58b8c8b965d7e3d7d6776b00be26b0
Egor <delont228322@gmail.com>
2026-06-13 23:03:19 +0300

    one_task.py

--- one_task.py
+++ one_task.py
@@ -1,8 +1,17 @@
-import google.generativeai as genai
+from google import genai
+from google.genai import types
+client = genai.Client (api_key="API")
 
-genai.configure(api_key="API")
-
-model = genai.GenerativeModel("gemini-3.5-flash")
-
-response = model.generate_content("Как тебя зовут?") #text
-print(response.text)
+try:
+    response = client.models.generate_content(
+        model="gemini-3.5-flash",
+        contents="", #promt
+        config=types.GenerateContentConfig(
+            system_instruction="", #text
+            temperature=0.8,
+            max_output_tokens=800 #so many words
+        )
+    )
+    print(response.text)
+except Exception as e:
+    print(f"Ошибка: {e}") #error operation
